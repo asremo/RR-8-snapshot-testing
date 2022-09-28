3454ef069a0aee787587ea1344ef7bee1d678599
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-27 22:10:02 -0400

    with snapshots and githubcard component

---
+++ src/GitHubCard/GitHubCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+import Woody from './woody.jpg'
+
+
+function GitHubCard() {
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Woody} />
+      <Card.Body>
+        <Card.Title>Woody</Card.Title>
+        <Card.Text>
+          I am Woody from the Toy Story movies.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default GitHubCard;
